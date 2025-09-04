53987f2f7f4110e26b7e5119619544538f022b95
Evandro Tomas <evandrotomas@hotmail.com>
2025-09-04 18:58:32 -0300

    feat: ensure task status con be changed

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -2,23 +2,21 @@ import CheckIcon from "../assets/icons/check.svg?react"
 import LoaderIcon from "../assets/icons/loader.svg?react"
 import DetailsIcon from "../assets/icons/details.svg?react"
 
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, handleTaskCheckboxClick }) => {
   const getStatusClasses = () => {
     if (task.status === "done") {
       return "bg-[#00ADB5]  text-[#00ADB5]"
     }
-
     if (task.status === "in_progress") {
       return "bg-[#FFAA04] text-[#FFAA04]"
     }
-
     if (task.status === "not_started") {
       return "bg-[#35383E] bg-opacity-10 text-[#35383E]"
     }
   }
   return (
     <div
-      className={`flex items-center justify-between gap-2 rounded-lg bg-opacity-10 px-4 py-3 ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg bg-opacity-10 px-4 py-3 transition ${getStatusClasses()}`}
     >
       <div className="flex items-center gap-2">
         <label
@@ -26,8 +24,9 @@ const TaskItem = ({ task }) => {
         >
           <input
             type="checkbox"
-            checked={task.status === "done"}
+            // checked={task.status === "done"}
             className="absolute h-full cursor-pointer opacity-0"
+            onChange={() => handleTaskCheckboxClick(task.id)}
           />
           {task.status === "done" && <CheckIcon />}
           {task.status === "in_progress" && (
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -10,11 +10,31 @@ import TASKS from "../constants/tasks"
 import TaskItem from "./TaskItem"
 
 const Tasks = () => {
-  const [tasks] = useState(TASKS)
+  const [tasks, setTasks] = useState(TASKS)
 
   const morningTasks = tasks.filter((task) => task.time === "morning")
   const afternoonTasks = tasks.filter((task) => task.time === "afternoon")
   const eveningTasks = tasks.filter((task) => task.time === "evening")
+
+  const handleTaskCheckboxClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id != taskId) {
+        return task
+      }
+      if (task.status === "not_started") {
+        return { ...task, status: "in_progress" }
+      }
+      if (task.status === "in_progress") {
+        return { ...task, status: "done" }
+      }
+      if (task.status === "done") {
+        return { ...task, status: "not_started" }
+      }
+
+      return task
+    })
+    setTasks(newTasks)
+  }
 
   return (
     <div className="w-full px-8 py-16">
@@ -44,7 +64,11 @@ const Tasks = () => {
           <TasksSeparator title="Manhã" icon={<SunIcon />} />
 
           {morningTasks.map((task) => (
-            <TaskItem key={task.id} task={task} />
+            <TaskItem
+              key={task.id}
+              task={task}
+              handleTaskCheckboxClick={handleTaskCheckboxClick}
+            />
           ))}
         </div>
 
@@ -52,7 +76,11 @@ const Tasks = () => {
           <TasksSeparator title="Tarde" icon={<CloudIcon />} />
 
           {afternoonTasks.map((task) => (
-            <TaskItem key={task.id} task={task} />
+            <TaskItem
+              key={task.id}
+              task={task}
+              handleTaskCheckboxClick={handleTaskCheckboxClick}
+            />
           ))}
         </div>
 
@@ -60,7 +88,11 @@ const Tasks = () => {
           <TasksSeparator title="Noite" icon={<MoonIcon />} />
 
           {eveningTasks.map((task) => (
-            <TaskItem key={task.id} task={task} />
+            <TaskItem
+              key={task.id}
+              task={task}
+              handleTaskCheckboxClick={handleTaskCheckboxClick}
+            />
           ))}
         </div>
       </div>
