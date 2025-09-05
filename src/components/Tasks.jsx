874754381aae5e4c80551f311391c0fcfe8472f7
Evandro Tomas <evandrotomas@hotmail.com>
2025-09-05 12:41:13 -0300

    feat: initial AddTaskDiallog component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -9,12 +9,14 @@ import {
   TrashIcon,
 } from "../assets/icons/"
 import TASKS from "../constants/tasks"
+import AddTaskDialog from "./AddTaskDialog"
 import Button from "./Button"
 import TaskItem from "./TaskItem"
 import TasksSeparator from "./TasksSeparator"
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS)
+  const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false)
 
   const morningTasks = tasks.filter((task) => task.time === "morning")
   const afternoonTasks = tasks.filter((task) => task.time === "afternoon")
@@ -65,10 +67,12 @@ const Tasks = () => {
             <TrashIcon />
           </Button>
 
-          <Button variant="primary">
+          <Button onClick={() => setAddTaskDialogIsOpen(true)}>
             Nova Tarefa
             <AddIcon />
           </Button>
+
+          <AddTaskDialog isOpen={addTaskDialogIsOpen} />
         </div>
       </div>
 
