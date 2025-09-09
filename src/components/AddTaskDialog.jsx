8f8ef4fed7e7ea47865441732558fc55349b5529
Evandro Tomas <evandrotomas@hotmail.com>
2025-09-09 15:19:02 -0300

    refactor: add design system

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -85,10 +85,10 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
           >
             {/* DIALOG */}
             <div className="rounded-xl bg-white p-5 text-center shadow">
-              <h2 className="text-xl font-semibold text-[#35383E]">
+              <h2 className="text-xl font-semibold text-brand-dark-blue">
                 Nova Tarefa
               </h2>
-              <p className="mb-4 mt-1 text-[#9A9C9F]">
+              <p className="mb-4 mt-1 text-brand-text-gray">
                 Insira as informações abaixo
               </p>
 
--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,9 +1,9 @@
 const SidebarButton = ({ children, variant }) => {
   const getVariantClasses = () => {
     if (variant === "unselected") {
-      return "text-[#35383E]"
+      return "text-brand-dark-blue"
     } else {
-      return "rounded-lg bg-[#E6F7F8]  text-[#00ADB5]"
+      return "rounded-lg bg-brand-primary bg-opacity-15  text-brand-primary"
     }
   }
 
