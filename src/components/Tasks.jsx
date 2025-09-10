0f79982d66235322b1ba9a365d1054c419df46ce
Evandro Tomas <evandrotomas@hotmail.com>
2025-09-10 19:27:37 -0300

    feat: ensure task is created on db

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -65,7 +65,17 @@ const Tasks = () => {
     setTasks(newTasks)
   }
 
-  const handleAddTaskSubmit = (task) => {
+  const handleAddTaskSubmit = async (task) => {
+    // chamar api para adcionar essa tarefa
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "post",
+      body: JSON.stringify(task),
+    })
+    if (!response.ok) {
+      return toast.error(
+        "Erro ao adicionar a tarefa. Por favor tente novamente.",
+      )
+    }
     setTasks([...tasks, task])
     toast.success("Tarefa adicionada com sucesso!")
   }
